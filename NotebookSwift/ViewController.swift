583dda6e0cc232bbdaf54ba08e3390b8f694cb8c
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-15 17:24:33 +0200

    Initial Commit

---
+++ NotebookSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotebookSwift
+//
+//  Created by Сергей Голенко on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
