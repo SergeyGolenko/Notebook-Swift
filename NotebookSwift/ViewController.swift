eb093a672acc02243172f3bde55cf58ea5fd6051
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-17 11:31:48 +0200

    finish v1

--- NotebookSwift/ViewController.swift
+++ NotebookSwift/ViewController.swift
@@ -8,50 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // cюда будут передаваться данные с ячейки(если в ней содержаться данные)
     var entry : Entries?
-    
-    
+    // переменная для доступа к NSManagedObjectContext
+    let contextDelegate = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     
     @IBAction func dateChanched(_ sender: UIDatePicker) {
         entry?.date = newTimeForNotebook.date
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-        
     }
-    
-    
+    // удаление данных
     @IBAction func trashButton(_ sender: UIBarButtonItem) {
         if entry != nil {
-            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let context = contextDelegate {
                 context.delete(entry!)
                 try? context.save()
             }
         }
         navigationController?.popViewController(animated: true)
     }
-    
+    //MARK: - Outlets
     @IBOutlet weak var newTimeForNotebook: UIDatePicker!
     @IBOutlet weak var newTextForNotebook: UITextView!
     @IBOutlet weak var constraintText: NSLayoutConstraint!
     
+    
+    //MARK:- viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //cоздаю наблюдателя для изминения размера textView, когда появляется клавиатура
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         
-        
+        //выполнить эти действия если в entry отсутствует значение
         if entry == nil {
-            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let context = contextDelegate {
                 entry = Entries(context: context)
                 entry?.date = newTimeForNotebook.date
                 entry?.text = "Today was..."
                 newTextForNotebook.becomeFirstResponder()
             }
         }
+        //Если в переменной entry есть данные, то нужно выполнить следующий код
         newTextForNotebook.text = entry?.text
         if let dateToBeShown = entry?.date{
             newTimeForNotebook.date = dateToBeShown
         }
+        
+        //подписываюсь на delegate, что бы отработал метод textViewDidChange
         newTextForNotebook.delegate = self
     }
     
@@ -68,20 +71,11 @@ class ViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-    
-    
-    
-    
 }
 
 extension ViewController: UITextViewDelegate{
-    
-    
     func textViewDidChange(_ textView: UITextView) {
         entry?.text = newTextForNotebook.text
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
-    
-    
-    
 }
