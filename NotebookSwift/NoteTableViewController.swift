f7b0eb9c60269b366b1dc55301b95044cd964b8f
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-17 11:08:32 +0200

    организация кода в первом контроллере

--- NotebookSwift/NoteTableViewController.swift
+++ NotebookSwift/NoteTableViewController.swift
@@ -10,6 +10,7 @@ import CoreData
 
 class NoteTableViewController: UITableViewController {
     
+    // массив содержащий объекты типа Entries
     var entriesArray : [Entries] = []
 
     override func viewDidLoad() {
@@ -18,11 +19,11 @@ class NoteTableViewController: UITableViewController {
 
     //Эта функция срабатывает при каждом появлении этого контроллера
     override func viewWillAppear(_ animated: Bool) {
+        
+        // при каждом появлении этого экрана, мы вытаскиваем из CoreData наши данные и присваиваем их entriesArray и перезагружаем таблицу
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
-            
             let request : NSFetchRequest<Entries> = Entries.fetchRequest()
             request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-            
             if let entriesFromCoreData =  try? context.fetch(request) {
                 entriesArray = entriesFromCoreData
                 self.tableView.reloadData()
@@ -43,6 +44,8 @@ class NoteTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // кастомную ячейку нужно привести к ее типу
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellCustom", for: indexPath) as? CustomCellTableViewCell
         let item = entriesArray[indexPath.row]
         cell?.myTextLabel.text = item.text
@@ -51,6 +54,7 @@ class NoteTableViewController: UITableViewController {
         return cell!
     }
     
+    // здесть через segue получаю доступ к другому контроллеру и передаю один выбранный объект Entries который был выбрал в методе didSelect и сохранен sender
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let secondVC = segue.destination as? ViewController{
             if let entryToBeSent = sender as? Entries {
@@ -59,6 +63,8 @@ class NoteTableViewController: UITableViewController {
         }
     }
     
+    
+    //здесь при нажатии на ячейку захватываю один объект Entries и передаю его через performSegue в sender
     //MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let entry = entriesArray[indexPath.row]
